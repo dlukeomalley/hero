ede1ef2aa30862ee58c9ba808d1b5b12033e083c
Red Developers <2009red@mit.edu>
2013-11-13 01:25:46 +0000

    Fixed variable naming issues in data collector, turned down sensitivity of mpr121

--- src/data_collector.py
+++ src/data_collector.py
@@ -11,8 +11,8 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(CAPSENSE_IRQ_PIN, GPIO.IN)
 
 # Initialize MP121 capacitive touch board
-mpr121.TOU_THRESH = 0x0F
-mpr121.REL_THRESH = 0x0A
+mpr121.TOU_THRESH = 0x15
+mpr121.REL_THRESH = 0x12
 mpr121.setup(MPR121_ADDR)
 
 # Loop
@@ -20,10 +20,11 @@ while True:
     sensor = [0] * NUM_TOUCH_PADS
     touch_data = mpr121.readData(MPR121_ADDR)
 
-    print bin(touch_data)
+    #print bin(touch_data)
 
     for i in range(NUM_TOUCH_PADS):
-        if touchData & (1 << i):
+        if touch_data & (1 << i):
             sensor[i] = 1
 
     print sensor
+    time.sleep(.5)
