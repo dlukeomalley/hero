d1f1762ff5872e271adfb6e4428698cb9e0df0d3
Luke O'Malley <omalley1@mit.edu>
2013-11-17 23:41:27 +0100

    Fix variable for data_set

--- src/basic_interpreter.py
+++ src/basic_interpreter.py
@@ -6,14 +6,13 @@ import rospy
 from hero.msg import CapSense, Action
 import pdb
 
-data = {(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0): None,
+data_set = {(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0): None,
         (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0): "BELLY UPPER",
         (0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0): "BELLY LOWER",
         (0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0): "LEFT ARM",
         (0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0): "RIGHT ARM",
         (0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0): "FRONT HEAD",
         (0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0): "BACK HEAD"}
-    
 
 class Interpreter():
     def __init__(self):
@@ -28,8 +27,8 @@ class Interpreter():
 
         rospy.loginfo("Basic Interpreter Read: {}".format(sensor))
 
-        distances = [(action, euclidean_distance(sensor, sample)) for sample, action in data.iteritems()]
-        distances = sorted(distances, key=lambda x: entry[1])
+        distances = [(action, self.euclidean_distance(sensor, sample)) for sample, action in data_set.iteritems()]
+        distances = sorted(distances, key=lambda pair: pair[1])
 
         action, val = distances[0]
 
@@ -39,10 +38,12 @@ class Interpreter():
     def publish(self, abstract_action):
         self.pub.publish(abstract_action)
 
-    def euclidean_distance(s1, s2):
+    def euclidean_distance(self, s1, s2):
         return sum([(x - y)**2 for x, y in zip(s1, s2)])
 
 if __name__ == '__main__':
-    rospy.init_node('basic_interpreter', anonymous=True)
-    Interpreter()
-    rospy.spin()
+    try:
+        rospy.init_node('basic_interpreter', anonymous=True)
+        Interpreter()
+        rospy.spin()
+    except rospy.ROSInterruptException: pass
