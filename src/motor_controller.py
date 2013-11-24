6689f31aa4d7f32eb18c6bd60b2c3a0074b13442
Luke O'Malley <omalley1@mit.edu>
2013-11-24 14:07:28 -0500

    Refactor motor code to have launch configure nodes

--- src/motor_controller.py
+++ src/motor_controller.py
@@ -13,31 +13,24 @@ from hero.msg import MotorCoordinate
 class Motor:
     # TODO: Include max_voltage so that avg voltage from PWM doesn't exceed ratings of motor
     def __init__(self): 
-        # , name, pins, limits, max_voltage=6, threshold=5):
-        # pins (pos, neg), limits (low, high)
-        self.name = 'test'
-        # self.pos, self.neg = pins
-        # self.low, self.high = limits
+        import pdb; pdb.set_trace()
         
-        # # how close we have to be to the goal before we stop
-        # self.threshold = threshold
-
-        # self.goal = None
-        # self.position = None
-
-        # # move motor to starting place
-        # self.move_to(0)
-
-        # Name node, subscribe to positions topic, and setup shutdown handle
-        rospy.init_node("{}_motor".format(self.name.lower(), anonymous=True))
-        
-        import pdb
-        pdb.set_trace()
-
-
+        rospy.init_node("motor_node".format(self.name.lower(), anonymous=True))
         rospy.Subscriber("goals", MotorCoordinate, self.goal_callback)
         rospy.Subscriber("locations", MotorCoordinate, self.update)
         rospy.on_shutdown(self.stop)
+
+        self.name = rospy.get_name().split(/)[-1].upper()
+        self.pos, self.neg = rospy.get_param("pins")
+        self.low, self.high = rospy.get_param("limits")
+        # defaults to 5 if not found
+        self.threshold = rospy.get_param('threshold', 5)
+
+        print "Name: {}".format(self.name)
+        
+        self.goal = 0
+        self.position = None
+        self.stop()
 
     def goal_callback(self, data):
         if data.name == self.name:
@@ -60,12 +53,8 @@ class Motor:
 
     def update(self, data):
         if data.name == self.name:
-            # to convert from 0-100 -> range
-            # position = self.low + (self.high - self.low)*data[self.name]/100.0
-            
-            # to convert from range -> 0-100
+            # remap ADC values to 0-100
             position = 100.0 * (data.position - self.low) / (self.high - self.low)
-            
             self.position = int(position)
             self.move_to(self.goal)
 
@@ -73,19 +62,5 @@ class Motor:
         servo.setMotorSpeed(self.pos, self.neg, 0)
 
 if __name__ == '__main__':
-    # DEFINITIONS
-    HEAD_PINS = (0,1)
-    LARM_PINS = (2,3)
-    RARM_PINS = (4,5)
-    BLINK_PINS = (6,7)
-
-    HEAD_LIMITS = (0, 1024)
-    LARM_LIMITS = (0, 1024)
-    RARM_LIMITS = (0, 1024)
-    BLINK_LIMITS = (0, 1024)
-
-    # CREATE MOTOR NODES. "HEAD", HEAD_PINS, HEAD_LIMITS)
-    # TODO: Do we want analog channel to report by channel or title?
     Motor()
-
     rospy.spin()
